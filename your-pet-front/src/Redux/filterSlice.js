e3f6c344af0fc4f33e464ca4d0e7da306c0e5ed6
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-05-11 11:52:09 +0600

    start

---
+++ your-pet-front/src/Redux/filterSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+        setFilter: (_, action) => action.payload,
+    },
+});
+
+export const { setFilter } = filterSlice.actions;
