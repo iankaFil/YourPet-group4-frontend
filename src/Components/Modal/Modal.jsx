3ff5fd24d0bbe8598b218faec2e10b75be49f6f3
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-05-13 18:38:27 +0600

    Update Modal.jsx

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 import { CrossSmallIcon } from './../SvgIcons/CrossSmallIcon';
 
-import css from './Modal.module.css'
-
+import css from './Modal.module.css';
 
 //этот код нужно добавитьо в компонент, в котором используется модалка
 // const [showModal, setShowModal] = useState(false);
@@ -19,45 +18,45 @@ import css from './Modal.module.css'
 //   }
 
 //рендеринг модалки в компоненте
-{/* <button onClick={handleOpenModal}>Open modal</button>
-{showModal && <Modal onClose={handleCloseModal}> 
-    Здесь будет ваш контент
-</Modal>} */}
 
-const Modal = ({ onClose, className, children="Modal content goes here" }) => { 
-    
-    useEffect(() => {
+const Modal = ({
+  onClose,
+  className,
+  children = 'Modal content goes here',
+}) => {
+  useEffect(() => {
     function handleKeyDown(event) {
-      if (event.keyCode === 27) { // escape key
+      if (event.keyCode === 27) {
+        // escape key
         onClose();
       }
     }
 
     function handleClick(event) {
-        if (event.target === event.currentTarget) {
-            onClose();
-        }
+      if (event.target === event.currentTarget) {
+        onClose();
+      }
     }
 
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('click', handleClick);
 
     return () => {
-        window.removeEventListener('keydown', handleKeyDown);
-        window.removeEventListener('click', handleClick);
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('click', handleClick);
     };
-    }, [onClose]);
-    
-    return (
-        <div className={`${css.modal} ${className}`}>
-            <div className={css.modalContent}>
-                <button className={css.modalBtn} onClick={onClose}>
-                    <CrossSmallIcon id='svg' className={css.crossSmallIcon}/>
-                </button>
-                    {children}
-                </div>
-        </div>
-    );
-}
+  }, [onClose]);
 
-export default Modal
+  return (
+    <div className={`${css.modal} ${className}`}>
+      <div className={css.modalContent}>
+        <button className={css.modalBtn} onClick={onClose}>
+          <CrossSmallIcon id="svg" className={css.crossSmallIcon} />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
