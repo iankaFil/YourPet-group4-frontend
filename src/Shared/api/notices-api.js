bdc297a9cdc3faf2c42de38d3351a8c0bc959a2b
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-05-13 19:06:39 +0300

    added api

---
+++ src/Shared/api/notices-api.js
@@ -0,0 +1,26 @@
+import instance from './auth-api';
+
+const CATEGORIES = ["sell", "in-good-hands", "lost-found"];
+
+
+export const getNoticesByCategory = async category => { 
+    if (!CATEGORIES.includes(category)) {
+    throw new Error(`Invalid category: ${category}. Allowed categories are: ${CATEGORIES.join(", ")}`);
+    }
+    
+    const { data } = await instance.get(`/notices/category/${category}`);
+    return data;
+};
+export const getNoticesByTitleInCategory = async (category,query) => {
+    if (!CATEGORIES.includes(category)) {
+    throw new Error(`Invalid category: ${category}. Allowed categories are: ${CATEGORIES.join(", ")}`);
+    }
+    
+    const { data } = await instance.get(`/notices/category/q=${query}`);
+    return data;
+};
+
+export const getNoticesById = async id => {
+    const { data } = await instance.get(`/notices/notice/${id}`);
+    return data;
+};
---
+++ src/Shared/api/pets-api.js
@@ -0,0 +1,11 @@
+import instance from './auth-api';
+
+export const addPet = async data => {
+  const { data: result } = await instance.post('/WRITE ENDPOINT', data);
+  return result;
+};
+
+export const deletePet = async id => {
+  const { data } = await instance.delete(`'/WRITE ENDPOINT/${id}`);
+  return data;
+};
