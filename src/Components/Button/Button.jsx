619192cf4d6bac6783ba82bfc08487f7bc5682e0
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-05-14 22:43:11 +0300

    update userPage, AuthForm

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import css from './Button.module.css'
 
-const Button = ({children, className, ...props}) => {
+const Button = ({children, className, type, ...props}) => {
     return (
-        <button className={`${css.button} ${className} `} >
+        <button type={ type} className={`${css.button} ${className} `} >
             {children}
         </button>
     )
