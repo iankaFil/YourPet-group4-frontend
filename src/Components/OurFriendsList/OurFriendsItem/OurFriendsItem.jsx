d08dd5cbd5da6036ca7e5493a7b4dd22f0d4eef6
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-05-15 16:58:56 +0600

    change homepage

--- src/Components/OurFriendsList/OurFriendsItem/OurFriendsItem.jsx
+++ src/Components/OurFriendsList/OurFriendsItem/OurFriendsItem.jsx
@@ -12,13 +12,17 @@ const OurFriendsItem = ({
 }) => {
   return (
     <li className={css.item} key={id}>
-      <img className={css.img} src={imgUrl} alt={title} loading={loading} />
-      <h3 className={css.title}>{title}</h3>
-      <p className={css.address}>{address}</p>
-      <p className={css.phone}>{phone}</p>
-      <a className={css.email} href={`mailto:${email}`}>
-        {email}
-      </a>
+      <div className={css.imgWrap}>
+        <img className={css.img} src={imgUrl} alt={title} loading={loading} />
+      </div>
+      <div className={css.descrWrap}>
+        <h3 className={css.title}>{title}</h3>
+        <p className={css.address}>{address}</p>
+        <p className={css.phone}>{phone}</p>
+        <a className={css.email} href={`mailto:${email}`}>
+          {email}
+        </a>
+      </div>
     </li>
   );
 };
