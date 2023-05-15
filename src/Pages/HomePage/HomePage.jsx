152c74e48282a221e21454bf8cb6ffb3a8e8af33
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-05-15 15:54:55 +0600

    add homepage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Section from 'Components/Section';
 import Container from 'Components/Container/';
+import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <Section>
-      <Container>HomePage</Container>
+    <Section className={css.background}>
+      <Container className={css.wrapp}>
+        <h1 className={css.title}>
+          Take good care {'\n'}of your small {'\n'}pets
+        </h1>
+      </Container>
     </Section>
   );
 };
