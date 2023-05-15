3c160f51b58a5845e2071d03e499d52563808a8e
Alexander Kushnir <dj.egregor1@gmail.com>
2023-05-15 11:23:29 +0200

    fix our friends bugs
    Fixed ourFriends unloading

--- src/Redux/ourFriends/ourFriends-operations.js
+++ src/Redux/ourFriends/ourFriends-operations.js
@@ -2,18 +2,17 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'https://your-pets-rest-api.onrender.com',
+  baseURL: 'https://your-pets-rest-api.onrender.com',
 });
 
 export const fetchOurFriends = createAsyncThunk(
-    'friends',
-    async (credentials, thunkAPI) => {
-        try {
-            const response = await instance.get('/sponsors', credentials);
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
+  'ourFriends/fetchOurFriends',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await instance.get('/sponsors', credentials);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
+  }
 );
-
