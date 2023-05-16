1e753976896692c144b3eab57cef547bd72b3a0c
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2023-05-16 11:14:21 +0300

    change notices search

--- src/Pages/NoticesPage/NoticesPage.jsx
+++ src/Pages/NoticesPage/NoticesPage.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import css from 'Pages/NoticesPage/NoticesPage.module.css';
+// import css from 'Pages/NoticesPage/NoticesPage.module.css';
+import Section from 'Components/Section';
 import Title from 'Components/Title/Title';
 import NoticesSearch from 'Components/Notices/NoticesSearch/NoticesSearch';
 import NoticesCategoriesNav from 'Components/Notices/NoticesFilters/NoticesFilters';
 import CategoryItem from 'Components/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem';
+import Container from 'Components/Container/Container';
 
 const NoticesPage = () => {
   return (
-    <div className={css.page_wrap}>
-      <Title>Find your favorite pet</Title>
-      <NoticesSearch />
-      <NoticesCategoriesNav />
-      <CategoryItem />
-    </div>
+    <Section>
+      <Container>
+        <Title>Find your favorite pet</Title>
+        <NoticesSearch />
+        <NoticesCategoriesNav />
+        <CategoryItem />
+      </Container>
+    </Section>
   );
 };
 
