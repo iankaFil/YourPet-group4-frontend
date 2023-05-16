8db625d641b80be7a4ef03fbb8825a0152eb8983
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2023-05-16 13:15:44 +0300

    change notices_list

--- src/Pages/NoticesPage/NoticesPage.jsx
+++ src/Pages/NoticesPage/NoticesPage.jsx
@@ -4,7 +4,8 @@ import Section from 'Components/Section';
 import Title from 'Components/Title/Title';
 import NoticesSearch from 'Components/Notices/NoticesSearch/NoticesSearch';
 import NoticesCategoriesNav from 'Components/Notices/NoticesFilters/NoticesFilters';
-import CategoryItem from 'Components/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem';
+import CategoryList from 'Components/Notices/NoticesCategoriesList/NoticesCategoriesList';
+// import CategoryItem from 'Components/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem';
 import Container from 'Components/Container/Container';
 
 const NoticesPage = () => {
@@ -14,7 +15,7 @@ const NoticesPage = () => {
         <Title>Find your favorite pet</Title>
         <NoticesSearch />
         <NoticesCategoriesNav />
-        <CategoryItem />
+        <CategoryList />
       </Container>
     </Section>
   );
