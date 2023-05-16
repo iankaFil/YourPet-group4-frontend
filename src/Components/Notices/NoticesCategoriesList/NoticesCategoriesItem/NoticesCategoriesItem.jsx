9eb5b6faa1e95976389abf56c5222ce3d21df38c
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2023-05-16 15:43:09 +0300

    change notices list

--- src/Components/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem.jsx
+++ src/Components/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem.jsx
@@ -5,26 +5,70 @@ const CategoryItem = () => {
     <ul className={css.card_list}>
       <li className={css.card_item}>
         <div className="card_wrap">
-          <img alt="default" className={css.image} />
-          <h2 className={css.title}>Сute cat looking for a home</h2>
+          <img
+            src="https://res.cloudinary.com/dynlclxi5/image/upload/v1684051457/ihjuoa4egh45ezt40foq.jpg"
+            alt="dog"
+            className={css.image}
+          />
+          <h2 className={css.title}>Сute dog looking for a home</h2>
           <button className={css.btn}>Learn more</button>
         </div>
       </li>
       <li className={css.card_item}>
         <div className="card_wrap">
-          <img alt="default" className={css.image} />
-          <h2 className={css.title}>Сute cat looking for a home </h2>
+          <img alt="dog" className={css.image} />
+          <h2 className={css.title}>Сute dog looking for a home </h2>
           <button className={css.btn}>Learn more</button>
         </div>
       </li>
       <li className={css.card_item}>
-        <img alt="default" className={css.image} />
-        <h2 className={css.title}>Сute cat looking for a home</h2>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
         <button className={css.btn}>Learn more</button>
       </li>
       <li className={css.card_item}>
-        <img alt="default" className={css.image} />
-        <h2 className={css.title}>Сute cat looking for a home</h2>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
+        <button className={css.btn}>Learn more</button>
+      </li>
+      <li className={css.card_item}>
+        <img alt="dog" className={css.image} />
+        <h2 className={css.title}>Сute dog looking for a home</h2>
         <button className={css.btn}>Learn more</button>
       </li>
     </ul>
