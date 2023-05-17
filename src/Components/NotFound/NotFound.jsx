441348da7fdaf46c27af8d11306c95e8fde2b6ce
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-05-17 20:24:22 +0600

    Update NotFound.jsx

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,10 +1,16 @@
 import css from './NotFound.module.css';
+import { useNavigate } from 'react-router-dom';
 // import myImage from '../../Shared/images/NotFoundImg/notFound-desktop@2x.png';
 import Button from 'Components/Button/Button';
 import { PawIcon } from '../SvgIcons/PawIcon';
 import Section from 'Components/Section/Section';
 
 export const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleToMain = () => {
+    navigate('/main');
+  };
   return (
     <Section className={css.background}>
       <main style={{ textAlign: 'center' }}>
@@ -12,7 +18,7 @@ export const NotFound = () => {
           <p className={css.text}>Ooops! This page not found :(</p>
           {/* <img src={myImage} alt="404" width="822px" height="360px" /> */}
           <div className={css.imgWrap}></div>
-          <Button className={css.button}>
+          <Button className={css.button} onClick={handleToMain}>
             To main page
             <PawIcon id="svg" />
           </Button>
