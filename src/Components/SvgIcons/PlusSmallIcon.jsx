31cc88e07f7122114f4d15a0513bb1448d9c8184
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2023-05-18 16:58:59 +0300

    change styles filter and addPat

--- src/Components/SvgIcons/PlusSmallIcon.jsx
+++ src/Components/SvgIcons/PlusSmallIcon.jsx
@@ -5,16 +5,21 @@ export const PlusSmallIcon = ({ id, className }) => {
     case 'svg':
       return (
         <svg
-            className={className}
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24" 
-            viewBox="0 0 24 24" 
-            fill='none'
+          className={className}
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
         >
-            <title>plus-small</title>
-            <path d="M12 7V17M7 12L17 12" stroke="#FEF9F9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          <title>plus-small</title>
+          <path
+            d="M12 7V17M7 12L17 12"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
         </svg>
       );
     default:
