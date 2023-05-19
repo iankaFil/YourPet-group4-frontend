d61b6d1bbc8a32c5acc5ca3449a7275a2fbb7731
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-05-19 18:23:41 +0600

    Update NoticesCategoriesItem.jsx

--- src/Components/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem.jsx
+++ src/Components/Notices/NoticesCategoriesList/NoticesCategoriesItem/NoticesCategoriesItem.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import css from './NoticesCategoriesItem.module.css';
 import { toast } from 'react-toastify';
