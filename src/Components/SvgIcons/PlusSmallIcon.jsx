b893d83419bd181e2d046a36cadde920553e3d7b
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2023-05-20 22:58:29 +0300

    change styles

--- src/Components/SvgIcons/PlusSmallIcon.jsx
+++ src/Components/SvgIcons/PlusSmallIcon.jsx
@@ -16,6 +16,7 @@ export const PlusSmallIcon = ({ id, className }) => {
           <title>plus-small</title>
           <path
             d="M12 7V17M7 12L17 12"
+            stroke="#FEF9F9"
             strokeWidth="1.5"
             strokeLinecap="round"
             strokeLinejoin="round"
